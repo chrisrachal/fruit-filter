866980b0e395a83321d4d92635b44fb3471620f5
chrisrachal <cjrachaljr@gmail.com>
2021-05-27 10:35:51 -0500

    components: Add Input and list

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    render() {
+        return (
+            <ul>
+                {/* list of fruits will go here */}
+            </ul>
+        )
+    }
+
+}
+
+export default List;
